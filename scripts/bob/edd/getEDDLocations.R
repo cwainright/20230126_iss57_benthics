aca3a5be25c4dc4ffde00996c576f97a0ce4a115
cwainright-nps <charles_wainright@nps.gov>
2023-01-31 13:52:55 -0500

    initial commit

--- scripts/bob/edd/getEDDLocations.R
+++ scripts/bob/edd/getEDDLocations.R
@@ -11,9 +11,9 @@ getEDDLocations <- function(results_list, habitat_marc2021, habitat_marc2022, bo
             
             #----- load project functions
             source("scripts/edd/getMarcHabLocations.R")
-            source("scripts/edd/getNCRNMacroinvertLocations.R")
-            source("scripts/edd/getNCRNHabLocations.R")
             source("scripts/edd/getNCRNChemLocations.R")
+            source("scripts/bob/edd/getNCRNMacroinvertLocations.R")
+            source("scripts/edd/getNCRNHabLocations.R")
             source("scripts/bob/edd/getBobMacroinvertLocations.R")
             source("scripts/bob/edd/getBobChemLocations.R")
             
@@ -21,22 +21,23 @@ getEDDLocations <- function(results_list, habitat_marc2021, habitat_marc2022, bo
             example <- readxl::read_excel("data/NCRN_BSS_EDD_20230105_1300.xlsx", sheet = "Locations") # https://doimspp.sharepoint.com/:x:/r/sites/NCRNDataManagement/Shared%20Documents/General/Standards/Data-Standards/EQuIS-WQX-EDD/NCRN_BSS_EDD_20230105_1300.xlsx?d=w8c283fde9cbd4af480945c8c8bd94ff6&csf=1&web=1&e=7Y9W1M
             
             #----- call NCRN project functions
-            ncrn_fish_locations <- getNCRNFishLocations(results_list, example)
-            ncrn_chem_locations <- getNCRNChemLocations(results_list, example)
-            ncrn_hab_locations <- getNCRNHabLocations(results_list, example)
             marc_habitat_locations <- getMarcHabLocations(habitat_marc2022, habitat_marc2021, example, results_list)
-            real <- rbind(ncrn_fish_locations, ncrn_chem_locations, ncrn_hab_locations, marc_habitat_locations) # row bind project function returns
+            ncrn_chem_locations <- getNCRNChemLocations(results_list, example)
+            ncrn_macroinvert_locations <- getNCRNMacroinvertLocations(results_list, example)
+            ncrn_hab_locations <- getNCRNHabLocations(results_list, example)
+            
+            real <- rbind(ncrn_macroinvert_locations, ncrn_chem_locations, ncrn_hab_locations, marc_habitat_locations) # row bind project function returns
             
             #----- if TRUE add Marc's 2022 data to dataframe `real` NCRN db data
             if(addBob==TRUE){
-              bob_2021_macroinvert_locations <- getBob2021MacroinvertsLocations()
-              bob_2021_chem_locations <- getBob2021ChemLocations()
+              bob_2021_macroinvert_locations <- getBob2021MacroinvertLocations(results_list, bob_2021_water_chem, example)
+              bob_2021_chem_locations <- getBob2021ChemLocations(results_list, bob_2021_water_chem, example)
               # combine
               real <- rbind(real, bob_2021_macroinvert_locations, bob_2021_chem_locations)
             }
             
             #----- keep only unique Locations
-            real <- real %>% distinct(Location_ID, .keep_all = TRUE)
+            real <- real[!duplicated(real[c('Latitude', 'Longitude')]),] # since naming conventions differed according to who did the sampling, we keep unique lat/lon pairs
             
             #----- error-checking:
             check_df <- tibble::tibble(data.frame(matrix(ncol=3, nrow=ncol(real))))
